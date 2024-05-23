b0b03b8e9eecf4199a873c1627237e30018f61d2
Dilawer Hussain <dh@imangu.com>
2024-05-23 18:00:21 +0500

    Created layout

--- sinCurve/src/app/components/charts/sineCurvedCharts/SineCurvedCharts.jsx
+++ sinCurve/src/app/components/charts/sineCurvedCharts/SineCurvedCharts.jsx
@@ -4,21 +4,21 @@ import { Box, Text } from '@chakra-ui/react'
 import colors from '../../../config/colors'
 import Icon from '../../form/Icon'
 import assets from '../../../assets/assests'
-import chartData from '../chartData'
+// import chartData from '../chartData'
 
-function SineCurvedCharts({ handleItemClick }) {
+function SineCurvedCharts({ chartData, handleItemClick }) {
   // console.log("SineCurvedCh ", chartData?.sineCurve?.rightFore);
   return (
-    <Box w='100%'>
+    <Box w='100%' p='16px'>
       <Box display={'flex'} gap={'29px'}>
-        <SineCurve color={colors.lightMediumBlue} chartData={chartData} data={chartData?.sineCurve?.leftFore} circle='Left circle' type='front' />
-        <SineCurve color={colors.lightPurple} chartData={chartData} data={chartData?.sineCurve?.rightFore} circle='right circle' type='front' />
-        <SineCurve color={colors.lightMustard} chartData={chartData} data={chartData?.sineCurve?.straightFore} circle='Straight' type='front' />
+        {chartData?.sineCurve?.leftFore?.length > 0 && <SineCurve color={colors.lightMediumBlue} chartData={chartData} data={chartData?.sineCurve?.leftFore} circle='Left circle' type='front' />}
+        {chartData?.sineCurve?.rightFore?.length > 0 && <SineCurve color={colors.lightPurple} chartData={chartData} data={chartData?.sineCurve?.rightFore} circle='right circle' type='front' />}
+        {chartData?.sineCurve?.straightFore?.length > 0 && <SineCurve color={colors.lightMustard} chartData={chartData} data={chartData?.sineCurve?.straightFore} circle='Straight' type='front' />}
       </Box>
       <Box display={'flex'} gap={'29px'}>
-        <SineCurve color={colors.lightMediumBlue} chartData={chartData} data={chartData?.sineCurve?.leftHind} type='hind' />
-        <SineCurve color={colors.lightPurple} chartData={chartData} data={chartData?.sineCurve?.rightHind} type='hind' />
-        <SineCurve color={colors.lightMustard} chartData={chartData} data={chartData?.sineCurve?.straighthind} type='hind' />
+        {chartData?.sineCurve?.leftHind?.length > 0 && <SineCurve color={colors.lightMediumBlue} chartData={chartData} data={chartData?.sineCurve?.leftHind} type='hind' />}
+        {chartData?.sineCurve?.rightHind?.length > 0 && <SineCurve color={colors.lightPurple} chartData={chartData} data={chartData?.sineCurve?.rightHind} type='hind' />}
+        {chartData?.sineCurve?.straighthind?.length > 0 && <SineCurve color={colors.lightMustard} chartData={chartData} data={chartData?.sineCurve?.straighthind} type='hind' />}
       </Box>
     </Box>
   )
