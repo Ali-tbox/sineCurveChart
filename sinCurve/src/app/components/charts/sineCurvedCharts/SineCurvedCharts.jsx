610b7f2e14b52fbb128087aafec5425e6b234819
Dilawer Hussain <dh@imangu.com>
2024-05-30 14:56:29 +0500

    Fixed baseline data

--- sinCurve/src/app/components/charts/sineCurvedCharts/SineCurvedCharts.jsx
+++ sinCurve/src/app/components/charts/sineCurvedCharts/SineCurvedCharts.jsx
@@ -15,7 +15,7 @@ function SineCurvedCharts({ chartData, baseline, date }) {
           <SineCurve
             color={colors.lightMediumBlue}
             chartData={chartData}
-            baseline={baseline?.sineCurve?.rightFore}
+            baseline={baseline?.sineCurve?.leftFore}
             date={date}
             data={chartData?.sineCurve?.leftFore}
             circle='Left circle'
@@ -23,29 +23,29 @@ function SineCurvedCharts({ chartData, baseline, date }) {
           />
         )}
         {chartData?.sineCurve?.rightFore?.length > 0 && (
+          <SineCurve color={colors.lightPurple} chartData={chartData} date={date} baseline={baseline?.sineCurve?.rightFore} data={chartData?.sineCurve?.rightFore} circle='Right circle' type='front' />
+        )}
+        {chartData?.sineCurve?.straightFore?.length > 0 && (
           <SineCurve
-            color={colors.lightPurple}
+            color={colors.lightMustard}
             chartData={chartData}
             date={date}
             baseline={baseline?.sineCurve?.straightFore}
-            data={chartData?.sineCurve?.rightFore}
-            circle='Right circle'
+            data={chartData?.sineCurve?.straightFore}
+            circle='Straight'
             type='front'
           />
-        )}
-        {chartData?.sineCurve?.straightFore?.length > 0 && (
-          <SineCurve color={colors.lightMustard} chartData={chartData} date={date} baseline={baseline?.sineCurve?.rightFore} data={chartData?.sineCurve?.straightFore} circle='Straight' type='front' />
         )}
       </Box>
       <Box display={'flex'} gap={'29px'}>
         {chartData?.sineCurve?.leftHind?.length > 0 && (
-          <SineCurve color={colors.lightMediumBlue} baseline={baseline?.sineCurve?.rightFore} chartData={chartData} date={date} data={chartData?.sineCurve?.leftHind} type='hind' />
+          <SineCurve color={colors.lightMediumBlue} baseline={baseline?.sineCurve?.leftHind} chartData={chartData} date={date} data={chartData?.sineCurve?.leftHind} type='hind' />
         )}
         {chartData?.sineCurve?.rightHind?.length > 0 && (
-          <SineCurve color={colors.lightPurple} baseline={baseline?.sineCurve?.rightFore} chartData={chartData} date={date} data={chartData?.sineCurve?.rightHind} type='hind' />
+          <SineCurve color={colors.lightPurple} baseline={baseline?.sineCurve?.rightHind} chartData={chartData} date={date} data={chartData?.sineCurve?.rightHind} type='hind' />
         )}
         {chartData?.sineCurve?.straighthind?.length > 0 && (
-          <SineCurve color={colors.lightMustard} baseline={baseline?.sineCurve?.rightFore} chartData={chartData} date={date} data={chartData?.sineCurve?.straighthind} type='hind' />
+          <SineCurve color={colors.lightMustard} baseline={baseline?.sineCurve?.straighthind} chartData={chartData} date={date} data={chartData?.sineCurve?.straighthind} type='hind' />
         )}
       </Box>
     </Box>
